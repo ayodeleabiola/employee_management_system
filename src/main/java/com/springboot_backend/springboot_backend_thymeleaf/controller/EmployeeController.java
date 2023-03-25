f6844694d274e222673ce669c1d9a83e751c292c
“Serbinmatosh” <milosmgmt@gmail.com>
2023-03-25 12:24:34 +0100

    [FIX] Refactored code an added additional functionalities

--- src/main/java/com/springboot_backend/springboot_backend_thymeleaf/controller/EmployeeController.java
+++ src/main/java/com/springboot_backend/springboot_backend_thymeleaf/controller/EmployeeController.java
@@ -4,36 +4,33 @@ import com.springboot_backend.springboot_backend_thymeleaf.model.Employee;
 import com.springboot_backend.springboot_backend_thymeleaf.service.imp.EmployeeServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("api/employee")
-public class Employee_Controller {
+public class EmployeeController {
+    /**
+     STOP naming your classes Employee_Controller -> NO UNDERSCORES IN JAVA!!!
+     */
+
     @Autowired
     private final EmployeeServiceImp employeeServiceImp;
 
-    public Employee_Controller( EmployeeServiceImp employeeServiceImp){
+    public EmployeeController(EmployeeServiceImp employeeServiceImp){
         this.employeeServiceImp = employeeServiceImp;
     }
 
-
     //display list of employee
     @GetMapping()
-    public String homeViewPage(Model model) {
-        model.addAttribute("listEmployees", employeeServiceImp.getAllEmployee());
-        return "index";
+    public ModelAndView homeViewPage() {
+        List<Employee> employees = employeeServiceImp.getAllEmployee();
+        ModelAndView modelAndView = new ModelAndView("employees.html");
+        modelAndView.addObject("employee", new Employee());
+        modelAndView.addObject("employees", employees);
+        return modelAndView;
     }
-
- /*
-    @GetMapping("")
-    public List<Employee> getAllEmployees(){
-        return employeeServiceImp.getAllEmployee();
-    }
-
-  */
 }
--- src/main/java/com/springboot_backend/springboot_backend_thymeleaf/service/imp/EmployeeServiceImp.java
+++ src/main/java/com/springboot_backend/springboot_backend_thymeleaf/service/imp/EmployeeServiceImp.java
@@ -1,20 +1,27 @@
 package com.springboot_backend.springboot_backend_thymeleaf.service.imp;
 
 import com.springboot_backend.springboot_backend_thymeleaf.model.Employee;
-import com.springboot_backend.springboot_backend_thymeleaf.repository.Employee_Repository;
+import com.springboot_backend.springboot_backend_thymeleaf.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 @Service
 public class EmployeeServiceImp {
 
-    private Employee_Repository employeeRepository;
-    public EmployeeServiceImp(Employee_Repository employeeRepository){
-        this.employeeRepository=employeeRepository;
+    private EmployeeRepository employeeRepository;
 
+    public EmployeeServiceImp(EmployeeRepository employeeRepository){
+        this.employeeRepository = employeeRepository;
     }
 
     public List<Employee> getAllEmployee(){
-        return employeeRepository.findAll();
+        return StreamSupport.stream(employeeRepository.findAll().spliterator(), false).collect(Collectors.toList());
+    }
+
+    public Employee addEmployee(Employee employee){
+        return employeeRepository.save(employee);
     }
 }
